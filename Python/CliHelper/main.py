8cd00df712e8b0c4c5d6cb025cc72c69764ffe69
Julian Freeman <freemanjulian372@gmail.com>
2023-04-03 18:20:11 -0400

    update Clihelper/main

--- Python/CliHelper/main.py
+++ Python/CliHelper/main.py
@@ -1,5 +1,5 @@
 # coding: utf8
-from clihelper import CliHelper
+from clihelper import CliHelper, request_input
 
 
 def open_file():
@@ -10,16 +10,28 @@ def new_folder(name):
     print(f"Folder [{name}] created")
 
 
-c = CliHelper()
-c.config(right_padding=20)
-n = c.add_option(title="New")
-c.add_option(title="Open File", func=open_file)
-c.add_option(title="Save")
-c.add_exit_option()
+def test_helper():
+    c = CliHelper()
+    c.config(right_padding=20, draw_menu_again=True)
+    n = c.add_option(title="New")
+    c.add_option(title="Open File", func=open_file)
+    c.add_option(title="Save")
+    c.add_exit_option()
 
-c.add_return_option(n)
-c.add_option(n, "New File")
-c.add_option(n, "New Folder", new_folder, ("Temp",))
-c.add_exit_option(n)
+    c.add_return_option(n)
+    c.add_option(n, "New File")
+    c.add_option(n, "New Folder", new_folder, ("Temp",))
+    c.add_exit_option(n)
 
-c.start_loop()
+    c.start_loop()
+
+
+def test_req():
+    v = request_input("\nEnter a number", func=lambda x: x.isdigit() and 1 < int(x) < 10, ask_again=True,
+                      has_default_val=True, default_val="4", need_confirm=True)
+    print(v)
+
+
+if __name__ == '__main__':
+    # test_req()
+    test_helper()
